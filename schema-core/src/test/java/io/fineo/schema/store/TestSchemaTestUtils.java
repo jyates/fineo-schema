5ba2c18056fb304111a9493d5a42613eb30b2d9d
Jesse Yates <jesse.k.yates@gmail.com>
2016-09-05 12:50:37 -0700

    Ensure that we are setting timestamp/writetime in SchemaTestUtils

--- schema-core/src/test/java/io/fineo/schema/store/TestSchemaTestUtils.java
+++ schema-core/src/test/java/io/fineo/schema/store/TestSchemaTestUtils.java
@@ -1,7 +1,12 @@
 package io.fineo.schema.store;
 
+import io.fineo.internal.customer.BaseFields;
 import org.apache.avro.generic.GenericRecord;
 import org.junit.Test;
+
+import static com.google.common.collect.Maps.newHashMap;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Because sometimes the tools get too complicated
@@ -10,6 +15,10 @@ public class TestSchemaTestUtils {
 
   @Test
   public void testCreateRandomRecord() throws Exception {
-    SchemaTestUtils.createRandomRecord();
+    GenericRecord record = SchemaTestUtils.createRandomRecord();
+    BaseFields base = (BaseFields) record.get(AvroSchemaProperties.BASE_FIELDS_KEY);
+    assertTrue(base.getTimestamp() > 0);
+    assertTrue(base.getWriteTime() > 0);
+    assertEquals(newHashMap(), base.getUnknownFields());
   }
 }
